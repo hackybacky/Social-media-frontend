33b100832d94412cd8dfc60c0182ac068420030a
hackybacky <hacky3244@gmail.com>
2022-02-07 19:32:37 +0530

    final commit

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import './login.css'
-export default function Login() {
+import './register.css'
+export default function Register() {
     return (
         <div className="login">
             <div className="loginWrapper">
                 <div className="loginLeft">
-                    <h3 className="loginLogo">Login compete</h3>
+                    <h3 className="loginLogo">Register Page </h3>
                     <span className="loginDesc">
                         connect with your favourite competetive coders 
                     </span>
                 </div>
                 <div className="loginRight">
                     <div className="loginBox">
+                        <input placeholder='Username' className="loginInput" />
                         <input placeholder='Email' className='loginInput' />
+                        
                         <input placeholder='Password' className="loginInput" />
-                        <button className='loginButton'> Log in </button>
-                        <span className="loginForgot"> Forgot login details</span>
-                        <button className="loginRegisterButton">Create new account </button>
+                        <input placeholder='Password Again' className="loginInput" />
+
+                        <button className='loginButton'> Sign up </button>
+                       
+                        <button className="loginRegisterButton">login into your account </button>
                     </div>
                 </div>
             </div>
